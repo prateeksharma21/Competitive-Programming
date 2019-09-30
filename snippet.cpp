e84df5448a3196f8c63412375aa28cc88483f7e3
Prateek Sharma <42744987+prateeksharma21@users.noreply.github.com>
2019-10-01 00:51:29 +0530

    Create Snippet.cpp

---
+++ snippet.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define F first
+#define S second
+#define mp make_pair
+#define pb push_back
+#define f(i,x,n) for(int i=x;i<n;i++)
+#define all(c) c.begin(),c.end()
+#define print(x) cerr << (#x) << "is "<< x << "\n"
+using ll = long long;
+using pii = pair<int,int>;
+const int MOD = 1e9+7, N = 1e5 + 10;
+ 
+void test();
+
+int32_t main(){
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	int tt = 1;
+	cin >> tt;
+	f(i,0,tt) test();	
+	return 0;
+}
+
+void test(){
+	$1
+}
