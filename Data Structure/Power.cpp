13fc3db50ae46e9f34445a1c7e1fd5c3b252c4ce
Prateek Sharma <you@example.com>
2020-04-25 19:13:50 +0530

    Added Modular Exponentiation

---
+++ Data Structure/Power.cpp
@@ -0,0 +1,15 @@
+long long power(long long x,long long y){
+   long long ans=1;
+   x %= MOD;
+   if(y < 0) return 0;
+   while(y){
+      if(y & 1) ans = (ans*x) %MOD;
+      x = x*x % MOD;
+      y /= 2;
+   }
+   return ans;
+}
+ 
+long long inv(long long x){
+   return power(x,MOD-2);
+}
