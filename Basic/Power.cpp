f3caddcda2cf22801309676823523d04f276adbf
Prateek Sharma <42744987+prateeksharma21@users.noreply.github.com>
2020-04-26 16:11:38 +0530

    Update Power.cpp

--- Basic/Power.cpp
+++ Basic/Power.cpp
@@ -2,7 +2,6 @@ long long power(long long x,long long y){
    long long ans=1;
    x %= MOD;
    if(y < 0) return 0;
-   if(y == 1) return ans;
    while(y){
       if(y & 1) ans = (ans*x) %MOD;
       x = x*x % MOD;
