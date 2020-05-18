39fe335b87bc6cddaa842cd7aa52a2b092acbfad
prateeksharma21 <prateeksharma21abc@gmail.com>
2020-05-18 18:52:51 +0530

    Added Point2D

---
+++ Data Structure/Point2D.cpp
@@ -0,0 +1,71 @@
+template <typename T>
+class Point2D
+{
+public:
+    T x, y;
+
+    Point2D()
+    {
+        x = 0, y = 0;
+    }
+
+    Point2D(T x, T y)
+    {
+        this->x = x;
+        this->y = y;
+    }
+
+    Point2D &operator+=(const Point2D &t)
+    {
+        x += t.x;
+        y += t.y;
+        return *this;
+    }
+
+    Point2D &operator-=(const Point2D &t)
+    {
+        x -= t.x;
+        y -= t.y;
+        return *this;
+    }
+
+    Point2D &operator*=(T t)
+    {
+        x *= t;
+        y *= t;
+        return *this;
+    }
+
+    Point2D &operator/=(T t)
+    {
+        assert(t != 0);
+        x /= t;
+        y /= t;
+        return *this;
+    }
+
+    Point2D operator+(const Point2D &t)
+    {
+        return Point2D(*this) += t;
+    }
+
+    Point2D operator-(const Point2D &t)
+    {
+        return Point2D(*this) -= t;
+    }
+
+    Point2D operator*(T t) const
+    {
+        return Point2D(*this) *= t;
+    }
+
+    Point2D operator/(T t) const
+    {
+        return Point2D(*this) /= t;
+    }
+
+    friend Point2D operator*(T a, Point2D b)
+    {
+        return b * a;
+    }
+};
