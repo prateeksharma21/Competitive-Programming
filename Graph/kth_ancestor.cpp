131dc42c861aa5b464ecf01b24d498fcf727e8c2
prateeksharma21 <prateeksharma21abc@gmail.com>
2020-06-27 03:50:09 +0530

    Updated Kth Ancestor

--- Graph/kth_ancestor.cpp
+++ Graph/kth_ancestor.cpp
@@ -1,37 +1,27 @@
 int parent[N][LG];
 
-void dfs(int u, int p = 0)
-{
+void dfs(int u, int p = 0){
     parent[u][0] = p;
-    for (auto &v : G[u])
-    {
-        if (v != p)
-        {
+    for (auto &v : G[u]){
+        if (v != p){
             dfs(v, u);
         }
     }
 }
 
-void precompute()
-{
-    f(j, 1, LG)
-    {
-        f(i, 1, n + 1)
-        {
-            if (parent[i][j - 1])
-            {
+void precompute(){
+    for(int j = 1; j < LG; ++j){
+        for(int i = 1; i <= n; ++i){
+            if (parent[i][j - 1]){
                 parent[i][j] = parent[parent[i][j - 1]][j - 1];
             }
         }
     }
 }
 
-int kth(int u, int d)
-{
-    for (int j = LG - 1; j >= 0; j--)
-    {
-        if ((1ll << j) <= d)
-        {
+int kth(int u, int d){
+    for (int j = LG - 1; j >= 0; j--){
+        if ((1ll << j) <= d){
             u = parent[u][j];
             d -= (1ll << j);
         }
