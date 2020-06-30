579b9f12037c50729d6038673065376d24cde94d
prateeksharma21 <prateeksharma21abc@gmail.com>
2020-06-30 18:38:54 +0530

    Changed Indentation in Snippet

--- Snippets/snippet.cpp
+++ Snippets/snippet.cpp
@@ -1,29 +1,29 @@
 #ifdef Prateek
-   #include "Prateek.h"
+  #include "Prateek.h"
 #else
-   #include <bits/stdc++.h>
-   using namespace std;
-   #define debug(...) 42
+  #include <bits/stdc++.h>
+  using namespace std;
+  #define debug(...) 42
 #endif
 #define F first
 #define S second
 #define pb push_back
 #define int ll
-#define f(i, x, n) for (int i = x; i < n; i++)
-#define all(c) c.begin(), c.end()
+#define f(i,x,n) for(int i=x;i<n;i++)
+#define all(c) c.begin(),c.end()
 using ll = long long;
-const int MOD = 1e9 + 7, N = 1e5 + 10;
+const int MOD = 1e9+7, N = 1e5 + 10;
 
 void test(){
-
-	return;
+   
+   return;
 }
 
 int32_t main(){
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	int tt = 1;
-	cin >> tt;
-	f(i, 0, tt) test();
-	return 0;
+   ios::sync_with_stdio(false);
+   cin.tie(NULL);
+   int tt = 1;
+   cin >> tt;
+   f(i,0,tt) test();   
+   return 0;
 }
