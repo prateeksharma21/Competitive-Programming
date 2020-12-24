2f0aca8ccb8bbccb775cdbd6731ab267cab8d54a
Prateek Sharma <prateeksharma21abc@gmail.com>
2020-12-24 21:52:26 +0530

    Modified Segment Tree

--- Library/Segment_Tree(Sum).cpp
+++ Library/Segment_Tree(Sum).cpp
@@ -1,49 +1,78 @@
-int A[N];
-int t[4*N];
-int lazy[4*N];
+template <typename T>
+class SegTreeSum {
+public:
+   vector<T> t, lazy, A;
+   int n;
 
-void build(int v,int tl,int tr){
-   if(tl == tr){
-      t[v] = A[tl];
-      return;
+   SegTreeSum (int n) {
+      this->n = n;
+      this->t.assign(4 * n + 10, 0);
+      this->lazy.assign(4 * n + 10, 0);
    }
-   int tm = (tl+tr)/2;
-   build(2*v,tl,tm);
-   build(2*v+1,tm+1,tr);
-   t[v] = t[2*v]+t[2*v+1];
-}
 
-void push(int v,int tl,int tr){
-   if(lazy[v]){
-      t[v] += (tr-tl+1)*lazy[v];
-      if(tl!=tr){
-         lazy[2*v] += lazy[v];
-         lazy[2*v+1] += lazy[v];
+   SegTreeSum (vector<T> a) : SegTreeSum (a.size()) {
+      this->A = a;
+      build(1, 0, n - 1);
+   }
+
+   void build (int v, int tl, int tr) {
+      if (tl == tr) {
+         t[v] = A[tl];
+         return;
       }
-      lazy[v] = 0;
+      int tm = (tl + tr) / 2;
+      build (v << 1, tl, tm);
+      build (v << 1 | 1, tm + 1, tr);
+      t[v] = t[v << 1] + t[v << 1 | 1];
    }
-}
 
-void update(int v,int tl,int tr,int l,int r,int val){
-   push(v,tl,tr);
-   if(tl>r or tr<l) return;
-   if(tl>=l and tr<=r){
-      lazy[v] += val;
-      push(v,tl,tr);
-      return;
+   void push (int v, int tl, int tr) {
+      if (lazy[v]) {
+         t[v] += (tr - tl + 1) * lazy[v];
+         if (tl != tr) {
+            lazy[v << 1] += lazy[v];
+            lazy[v << 1 | 1] += lazy[v];
+         }
+         lazy[v] = 0;
+      }
    }
-   int tm = (tl+tr)/2;
-   update(2*v,tl,tm,l,r,val);
-   update(2*v+1,tm+1,tr,l,r,val);
-   t[v] = t[2*v]+t[2*v+1];
-}
 
-int query(int v,int tl,int tr,int l,int r){
-   if(tr<l or tl>r) return 0;
-   push(v,tl,tr);
-   if(tl>=l and tr<=r){
-      return t[v];
+   void update (int v, int tl, int tr, int l, int r, T val) {
+      push (v, tl, tr);
+      if (tl > r or tr < l) return;
+      if (tl >= l and tr <= r) {
+         lazy[v] += val;
+         push (v, tl, tr);
+         return;
+      }
+      int tm = (tl + tr) / 2;
+      update (v << 1, tl, tm, l, r, val);
+      update (v << 1 | 1, tm + 1, tr, l, r, val);
+      t[v] = t[v << 1] + t[v << 1 | 1];
    }
-   int tm = (tl+tr)/2;
-   return query(2*v,tl,tm,l,r)+query(2*v+1,tm+1,tr,l,r);
-}
+
+   T query (int v, int tl, int tr, int l, int r) {
+      if (tr < l or tl > r) return 0;
+      push (v, tl, tr);
+      if (tl >= l and tr <= r) return t[v];
+      int tm = (tl + tr) / 2;
+      return query (v << 1, tl, tm, l, r) + 
+             query (v << 1 | 1, tm + 1, tr, l, r);
+   }
+
+   T query (int l, int r) {
+      return query (1, 0, n - 1, l, r);
+   }
+
+   T query (int p) {
+      return query (1, 0, n - 1, p, p);
+   }
+
+   void update (int l, int r, T val) {
+      update (1, 0, n - 1, l, r, val);
+   }
+
+   void update (int p, T val) {
+      update (1, 0, n - 1, p, p, val);
+   }
+};
--- Library/Segment_Tree.cpp
+++ Library/Segment_Tree.cpp
@@ -1,86 +1,84 @@
-template <typename T>
+template <typename T, bool max_mode = true>
 class SegTree{
 public:
-   vector<T> t,lazy;
-   vector<T> A;
+   vector<T> t, lazy, A;
    int n;
 
-   SegTree(int n){
+   SegTree (int n) {
       this->n = n;
-      this->t.assign(4*n+10,0);
-      this->lazy.assign(4*n+10,0);
+      this->t.assign(4 * n + 10, 0);
+      this->lazy.assign(4 * n + 10, 0);
    }
 
-   SegTree(vector<T> a){
-      this->n = a.size();
+   SegTree (vector<T> a) : SegTree (a.size()) {
       this->A = a;
-      this->t.assign(4*n,0);
-      this->lazy.assign(4*n,0);
-      build(1,0,n-1);
+      build (1, 0, n - 1);
    }
 
-   T fun(T a,T b){
-      
+   T op (T a,T b){
+      if (max_mode) return max(a, b);
+      return min(a, b);
    }
 
-   void build(int v,int tl,int tr){
-      if(tl == tr){
+   void build (int v,int tl,int tr){
+      if (tl == tr) {
          t[v] = A[tl];
-      }else{
-         int tm = (tl+tr)/2;
-         build(2*v,tl,tm);
-         build(2*v+1,tm+1,tr);
-         t[v] = fun(t[2*v],t[2*v+1]);
+      } else {
+         int tm = (tl + tr) / 2;
+         build (v << 1, tl, tm);
+         build (v << 1 | 1, tm + 1, tr);
+         t[v] = op (t[v << 1], t[v << 1 | 1]);
       }
    }
 
-   void push(int v) {
-      t[v*2] += lazy[v];
-      lazy[v*2] += lazy[v];
-      t[v*2+1] += lazy[v];
-      lazy[v*2+1] += lazy[v];
+   void push (int v) {
+      t[v << 1] += lazy[v];
+      lazy[v << 1] += lazy[v];
+      t[v << 1 | 1] += lazy[v];
+      lazy[v << 1 | 1] += lazy[v];
       lazy[v] = 0;
    }
 
-   void update(int v, int tl, int tr, int l, int r, T addend) {
-      if (l > r) 
-         return;
+   void update (int v, int tl, int tr, int l, int r, T addend) {
+      if (l > r) return;
       if (l == tl && tr == r) {
         t[v] += addend;
         lazy[v] += addend;
       } else {
-        push(v);
+        push (v);
         int tm = (tl + tr) / 2;
-        update(v*2, tl, tm, l, min(r, tm), addend);
-        update(v*2+1, tm+1, tr, max(l, tm+1), r, addend);
-        t[v] = fun(t[v*2], t[v*2+1]);
+        update (v << 1, tl, tm, l, min(r, tm), addend);
+        update (v << 1 | 1, tm + 1, tr, max(l, tm + 1), r, addend);
+        t[v] = op (t[v << 1], t[v << 1 | 1]);
       }
    }
 
-   T query(int v, int tl, int tr, int l, int r) {
-      if (l > r)
-        return ; // Value
+   T query (int v, int tl, int tr, int l, int r) {
+      if (l > r) {
+         if (max_mode) return -1e18;
+         return 1e18;
+      }
       if (l <= tl && tr <= r)
         return t[v];
-      push(v);
+      push (v);
       int tm = (tl + tr) / 2;
-      return fun(query(v*2, tl, tm, l, min(r, tm)), 
-               query(v*2+1, tm+1, tr, max(l, tm+1), r));
+      return op (query (v << 1, tl, tm, l, min(r, tm)), 
+                query (v << 1 | 1, tm+1, tr, max(l, tm+1), r));
    }
 
-   void update(int l,int r,T val){
-      update(1,0,n-1,l,r,val);
+   void update (int l, int r, T val){
+      update (1, 0, n-1, l, r, val);
    }
 
-   void update(int i,T val){
-      update(i,i,val);
+   void update (int i, T val) {
+      update (i, i, val);
    }
 
-   T query(int l,int r){
-      return query(1,0,n-1,l,r);
+   T query (int l,int r) {
+      return query (1, 0, n-1, l, r);
    }
 
-   T query(int i){
-      return query(i,i);
+   T query (int i) {
+      return query (i, i);
    }
 };
